86468503da9c487b71e6e118b5e60b2d4d2958a1
Jacky HE <ihkk.nb@gmail.com>
2025-05-14 13:42:49 +0800

    add random number

--- routes/memo.js
+++ routes/memo.js
@@ -4,6 +4,35 @@ const router = express.Router();
 let memos = []; // 内存中的假数据
 let idCounter = 1;
 
+function generateReadableId() {
+    const repeatCount = Math.floor(Math.random() * 2) + 2; // 2 或 3
+    const repeatDigit = Math.floor(Math.random() * 10).toString(); // 要重复的数字
+
+    let digits = [];
+
+    // 先放入重复的数字
+    for (let i = 0; i < repeatCount; i++) {
+        digits.push(repeatDigit);
+    }
+
+    // 再加入其他随机不重复的数字，直到长度为6
+    while (digits.length < 6) {
+        const rand = Math.floor(Math.random() * 10).toString();
+        if (rand !== repeatDigit || digits.filter(d => d === rand).length < repeatCount) {
+            digits.push(rand);
+        }
+    }
+
+    // 打乱顺序
+    for (let i = digits.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [digits[i], digits[j]] = [digits[j], digits[i]];
+    }
+
+    return digits.join('');
+}
+
+
 // 获取所有备忘录
 router.get('/', (req, res) => {
     res.json(memos);
@@ -12,7 +41,7 @@ router.get('/', (req, res) => {
 // 创建新备忘录
 router.post('/', (req, res) => {
     const { content } = req.body;
-    const newMemo = { id: idCounter++, content };
+    const newMemo = { id: generateReadableId(), content };
     memos.push(newMemo);
     res.status(201).json(newMemo);
 });
